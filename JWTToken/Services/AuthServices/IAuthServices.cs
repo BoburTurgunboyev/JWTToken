969fc9063fef75ce2eeeb27a4c082f6f0e36f855
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 19:04:36 +0500

    ozgarish bor

---
+++ JWTToken/Services/AuthServices/IAuthServices.cs
@@ -0,0 +1,9 @@
+﻿using JWTToken.Entities.Dtos;
+
+namespace JWTToken.Services.AuthServices
+{
+    public interface IAuthServices
+    {
+        public string GenerateToken(UsersDto usersDto);
+    }
+}
