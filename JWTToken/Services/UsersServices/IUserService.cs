0d4b668cdb01ed06a4becb1b3728913c31315602
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 19:04:22 +0500

    ozgarish bor

--- JWTToken/Services/UsersServices/IUserService.cs
+++ JWTToken/Services/UsersServices/IUserService.cs
@@ -1,6 +1,11 @@
-﻿namespace JWTToken.Services.UsersServices
+﻿using JWTToken.Entities;
+using JWTToken.Entities.Dtos;
+
+namespace JWTToken.Services.UsersServices
 {
     public interface IUserService
     {
+        public ValueTask<UsersDto> CreateUsersAsync(UsersDto usersDto);
+        public ValueTask<IEnumerable<Users>> GetUsersAsync();
     }
 }
