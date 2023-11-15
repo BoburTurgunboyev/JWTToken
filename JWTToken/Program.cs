83f2f34eb5ae5d03b9a0f36c380fba198d6d8ade
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 15:48:11 +0500

    Ozgarish

--- JWTToken/Program.cs
+++ JWTToken/Program.cs
@@ -1,6 +1,12 @@
+using JWTToken.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("Connect")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
