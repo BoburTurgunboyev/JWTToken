7f4cb0fd85f0b234be355300c683e28cfbe57571
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 15:46:55 +0500

    Ozgarish

---
+++ JWTToken/DataAccess/AuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿using JWTToken.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTToken.DataAccess
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+        public DbSet<Users> users { get; set; }
+
+    }
+}
