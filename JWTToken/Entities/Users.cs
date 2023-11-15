ec6024d98270086cb400c54ee4f8158e43524016
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 19:05:33 +0500

    ozgarish bor

--- JWTToken/Entities/Users.cs
+++ JWTToken/Entities/Users.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
+        public string Role { get; set; }
     }
 }
