4e484db9192117404b29fe7c85fb5f7ceebf8fb6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 19:03:46 +0500

    ozgarish bor

---
+++ JWTToken/Entities/Dtos/UsersDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTToken.Entities.Dtos
+{
+    public class UsersDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
