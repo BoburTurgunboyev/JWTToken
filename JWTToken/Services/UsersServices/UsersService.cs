3456342144f63c50c9ddb67b7832515371b510b3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-15 19:04:14 +0500

    ozgarish bor

--- JWTToken/Services/UsersServices/UsersService.cs
+++ JWTToken/Services/UsersServices/UsersService.cs
@@ -1,6 +1,35 @@
-﻿namespace JWTToken.Services.UsersServices
+﻿using JWTToken.DataAccess;
+using JWTToken.Entities;
+using JWTToken.Entities.Dtos;
+using JWTToken.Services.Hash;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTToken.Services.UsersServices
 {
-    public class UsersService
+    public class UsersService : IUserService
     {
+        private AuthDbContext _authDbContext;
+
+        public UsersService(AuthDbContext authDbContext)
+        {
+            _authDbContext = authDbContext;
+        }
+        public async ValueTask<UsersDto> CreateUsersAsync(UsersDto usersDto)
+        {
+            Users users = new Users();
+            users.UserName = usersDto.UserName;
+            users.PasswordHash = Hash512.ComputeHash512(usersDto.Password);
+            users.Role = usersDto.Role;
+            await _authDbContext.AddAsync(users);
+            await _authDbContext.SaveChangesAsync();
+            return usersDto;
+
+        }
+
+        public async ValueTask<IEnumerable<Users>> GetUsersAsync()
+        {
+            IEnumerable<Users> users = await _authDbContext.Users.ToListAsync();
+            return users;
+        }
     }
 }
